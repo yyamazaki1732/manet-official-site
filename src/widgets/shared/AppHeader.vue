edcf837ab4f3f45f90ebc030ae620790b7d303ba
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-20 21:54:52 +0900

    feat: 仮のヘッダーとフッターを作成

---
+++ src/widgets/shared/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <header :class="['header']">
+        <h1>App Header</h1>
+    </header>
+</template>
+
+<style scoped>
+.header {
+    background-color: #213141;
+    color: white;
+    padding: 10px;
+    text-align: center;
+    border-bottom: 1px solid #dee2e6;
+    min-height: 30px;
+}
+</style>
