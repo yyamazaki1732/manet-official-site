54f0cffe9604a98c555d840a1b3e487bfef03b7f
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-20 09:25:23 +0900

    feat: sharedディレクトリのパスをnuxt.configに設置した

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   alias: {
-    "@": '../src'
+    "@": '../src',
+    "#shared": "../src/shared",
   },
   dir: {
-    pages: './src/app/routes'
+    pages: './src/app/routes',
   }
 })
