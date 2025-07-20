b6222824f2039fb9e581915a2e34b8d49aed3f38
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-20 21:55:10 +0900

    feat: そのヘッダーとフッターをdefault.vueに追加

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
+        <AppHeader />
         <h2>main</h2>
         <slot />
+        <AppFooter />
     </div>
 </template>
+
+<script setup lang="ts">
+import AppHeader from '~/widgets/shared/AppHeader.vue';
+import AppFooter from '~/widgets/shared/AppFooter.vue';
+</script>
