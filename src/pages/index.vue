f0f56bedf7f342296aadf43e04b272fe451d208a
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-29 22:14:58 +0900

    test: 試しにNuxtImgをpages/index.vueに設定

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>index.vue</h1>
+    <NuxtImg v-slot="{ src, isLoaded, imgAttrs }" src="/img/paypay.png" alt="image" width="400" height="400"
+        :custom="true">
+        <!-- Show the actual image when loaded -->
+        {{ isLoaded }}
+        <img v-if="isLoaded" v-bind="imgAttrs" :src="src">
+
+        <!-- Show a placeholder while loading -->
+        <img v-else src="https://placehold.co/400x400" alt="placeholder">
+    </NuxtImg>
 </template>
