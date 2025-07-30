57c89b3b7872f86516475c46cddfc5091e4a2ffd
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-30 22:24:21 +0900

    add: useGlobalNavを作成

---
+++ src/shared/composables/useGlobalNav.ts
@@ -0,0 +1,12 @@
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+export function useGlobalNav() {
+  return {
+    isOpen,
+    open: () => { isOpen.value = true },
+    close: () => { isOpen.value = false },
+    toggle: () => { isOpen.value = !isOpen.value },
+  }
+}
