3ad09c634553e1863dc594e4f2df73e6d1e0e6b2
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-30 10:06:25 +0900

    feat: NuxtImgをNuxtPictureに変更

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,9 @@
 <template>
     <h1>index.vue</h1>
-    <NuxtImg v-slot="{ src, isLoaded, imgAttrs }" src="/img/paypay.png" alt="image" width="400" height="400"
-        :custom="true">
-        <!-- Show the actual image when loaded -->
-        {{ isLoaded }}
-        <img v-if="isLoaded" v-bind="imgAttrs" :src="src">
-
-        <!-- Show a placeholder while loading -->
-        <img v-else src="https://placehold.co/400x400" alt="placeholder">
-    </NuxtImg>
+    <NuxtPicture :img-attrs="{
+        'id': 'my-id',
+        'class': 'my-class',
+        'style': 'display: block',
+        'data-my-data': 'my-value',
+    }" format="webp" src="/img/paypay.png" alt="image" width="400" height="400" />
 </template>
