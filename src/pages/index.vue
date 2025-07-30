9e5c463f2454ab19912e1e28bd01448f18696cf0
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-07-30 21:48:58 +0900

    feat: 管理画面上の画像をNuxtPictureで表示させた

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,10 @@
 <template>
     <h1>index.vue</h1>
+
     <NuxtPicture :img-attrs="{
         'id': 'my-id',
         'class': 'my-class',
         'style': 'display: block',
         'data-my-data': 'my-value',
-    }" format="webp" src="/img/paypay.png" alt="image" width="400" height="400" />
+    }" format="webp" :src="$t('home.img.url')" alt="image" width="400" height="400" />
 </template>
