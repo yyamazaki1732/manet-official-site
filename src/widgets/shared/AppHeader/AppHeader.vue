ee567467f80a1bb107e0ab7bc22451c5bc579fc0
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-01 08:47:02 +0900

    feat: AppHeaderの切り替えをNuxtLinkに変更

--- src/widgets/shared/AppHeader/AppHeader.vue
+++ src/widgets/shared/AppHeader/AppHeader.vue
@@ -2,15 +2,13 @@
   <header :class="['header']">
     <h1>App Header / 'welcome'<br>{{ $t('home.subject') }}<br>{{ $t('about.subject') }}</h1>
     <div class="locales">
-      <button
-        v-for="(locale, index) in locales"
+      <NuxtLink
+        v-for="locale in locales"
         :key="locale.code"
-        type="button"
-        @click="setLocale(locale.code)"
+        :to="switchLocalePath(locale.code)"
       >
-        {{ locale.code }} <span v-if="index < locales.length - 1">-</span>
-        {{ locale.name }}
-      </button>
+        <h2> {{ locale.code }}</h2>
+      </NuxtLink>
     </div>
     <h2>isOpen : {{ isOpen }}</h2>
     <button @click="toggle">
@@ -20,7 +18,8 @@
 </template>
 
 <script setup lang="ts">
-const { locales, setLocale } = useI18n()
+const { locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
 const { isOpen, toggle } = useGlobalNav()
 </script>
 
