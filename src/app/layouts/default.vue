e7e775e9e182545c18850957b27c153f44f58d1e
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-04 22:18:24 +0900

    wip: deafult.vueにuseSeoMetaを設定中

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -7,3 +7,14 @@
     <UiFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: $t('common.ogp.url'),
+  twitterCard: 'summary_large_image',
+})
+</script>
