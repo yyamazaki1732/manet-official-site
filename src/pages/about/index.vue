d01d78bd70b36f7807cdac74d4c4b35811dbbaec
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-09 07:26:48 +0900

    feat: default, about/index でuseJsonldを設置

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -1,4 +1,30 @@
 <script setup lang="ts">
+import type { WithContext, BreadcrumbList } from 'schema-dts'
+
+const breadcrumbs = [
+  { url: 'https://example.com', text: 'top page' },
+  { url: 'https://example.com/foo', text: 'foo' },
+  { url: 'https://example.com/foo/bar', text: 'bar' },
+]
+
+function jsonld(): WithContext<BreadcrumbList> {
+  const items = breadcrumbs.map((item, index) => ({
+    '@type': 'ListItem' as const,
+    'position': index + 1,
+    'item': {
+      '@id': item.url,
+      'name': item.text,
+    },
+  }))
+  return {
+    '@context': 'https://schema.org',
+    '@type': 'BreadcrumbList',
+    'itemListElement': items,
+  }
+}
+
+useJsonld(jsonld())
+
 useSeoMeta({
   title: $t('about.subject'),
   ogTitle: $t('about.subject'),
