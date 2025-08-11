7a3cedadbbe344e42e7da0b5dfd5e33ae95f4dc9
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-12 07:42:18 +0900

    chore: news, postのAPIをフェッチした

---
+++ src/entities/api/model/post.d.ts
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+export interface PostItem {
+    post: Post;
+}
+
+export interface Post {
+    topics_id:                number;
+    ymd:                      Date;
+    contents_type:            number;
+    subject:                  string;
+    topics_flg:               number;
+    open_flg:                 number;
+    regular_flg:              number;
+    inst_ymdhi:               Date;
+    update_ymdhi:             Date;
+    topics_group_id:          number;
+    slug:                     string;
+    order_no:                 number;
+    col_sort:                 string;
+    group_nm:                 string;
+    group_description:        string;
+    contents_type_cnt:        number;
+    contents_type_nm:         string;
+    contents_type_slug:       string;
+    contents_type_parent_nm:  null;
+    category_parent_id:       null;
+    contents_type_ext_col_01: string;
+    contents_type_ext_col_02: string;
+    contents_type_ext_col_03: string;
+    contents_type_ext_col_04: string;
+    contents_type_ext_col_05: string;
+    contents_type_list:       number[];
+    meta_description:         string;
+    content:                  string;
+}
