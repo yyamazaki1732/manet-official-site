a53d9fb7de7b754ee921c4aff3d48540257013a0
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-11 19:48:13 +0900

    feat: footerのスタイルを作成

--- src/widgets/shared/Footer/Footer.vue
+++ src/widgets/shared/Footer/Footer.vue
@@ -4,19 +4,21 @@ import { globalNavItems } from '@/entities/navigation'
 
 <template>
   <footer :class="['footer', 'opacity-75']">
-    <h1>App Footer</h1>
-    <nav>
-      <ul>
-        <li
-          v-for="item in globalNavItems"
-          :key="item.name"
-        >
-          <NuxtLinkLocale :to="item.path">
-            {{ $t(`${item.name}.subject`) }}
-          </NuxtLinkLocale>
-        </li>
-      </ul>
-    </nav>
+    <h1 :class="['title']">
+      Footer
+    </h1>
+    <ul :class="['list']">
+      <li
+        v-for="item in globalNavItems"
+        :key="item.name"
+      >
+        <NuxtLinkLocale :to="item.path">
+          {{ $t(`${item.name}.subject`) }}
+        </NuxtLinkLocale>
+      </li>
+    </ul>
+    <small :class="['copyright', 'text-center']">© Copyright 2006 - 2025 Ochanomizu Christian Center All Rights Reserved.
+    </small>
   </footer>
 </template>
 
