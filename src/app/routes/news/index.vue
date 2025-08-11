f7c3d9025a7bae0b0247b539783f45b17e98acbe
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-12 07:45:16 +0900

    feat: newsページのコンポーネントを作成

---
+++ src/app/routes/news/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Page from '@/pages/about/index.vue'
+
+useSeoMeta({
+  title: $t('news.subject'),
+  ogTitle: $t('news.subject'),
+  description: $t('news.meta_description'),
+  ogDescription: $t('news.meta_description'),
+})
+</script>
+
+<template>
+  <div>
+    <Page />
+    <JsonLDNews />
+  </div>
+</template>
