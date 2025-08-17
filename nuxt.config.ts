906b90e4e4ecbe6e22b850555351ad478c771c7c
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-17 17:50:07 +0900

    feat: apiBaseURLとstaticTokenをnuxt.configに追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -40,6 +40,8 @@ export default defineNuxtConfig({
   css: ['@/app/style/base.css', '@/app/style/tailwindcss.css'],
   runtimeConfig: {
     public: {
+      apiBaseURL: 'https://manet.g.kuroco.app',
+      staticToken: '0c459601f46025f5ff46e57bb7605933065de5b0a31e7abd6509a448856d9229',
     },
   },
   dir: {
