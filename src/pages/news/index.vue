4e28d3a8db34a1aee36b199c2a25139108530b0a
YAMAZAKI <125118119+yyamazaki1732@users.noreply.github.com>
2025-08-17 18:24:49 +0900

    feat: listのスタイルを作成

--- src/pages/news/index.vue
+++ src/pages/news/index.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
 import jaPostFiles from '@/shared/i18n/locales/ja/post.json'
 
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import { computed, onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
 const totalCnt = jaPostFiles.post.pageInfo.totalCnt
 const perPage = jaPostFiles.post.pageInfo.perPage
 const totalPageCnt = jaPostFiles.post.pageInfo.totalPageCnt
-const firstIndex = jaPostFiles.post.pageInfo.firstIndex
-const lastIndex = jaPostFiles.post.pageInfo.lastIndex
-const startPageNo = jaPostFiles.post.pageInfo.startPageNo
-const endPageNo = jaPostFiles.post.pageInfo.endPageNo
 
 const route = useRoute()
+const router = useRouter()
 const currentPage = computed(() => {
   const page = Number(route.query.page)
   return !isNaN(page) && page >= 1 ? page : 1
+})
+
+onMounted(() => {
+  // pageパラメータが無い場合は?page=1にリダイレクト
+  if (!('page' in route.query)) {
+    router.replace({
+      path: route.path,
+      query: { ...route.query, page: 1 },
+    })
+  }
 })
 
 const startIndex = computed(() => (currentPage.value - 1) * perPage)
@@ -24,27 +31,26 @@ const pageIndexes = computed(() => Array.from({ length: endIndex.value - startIn
 </script>
 
 <template>
-  <div class="surface-color-quaternary">
+  <div class="surface-color-quaternary p-4">
     <h1>News.vue</h1>
-    <br>
-    <p>totalCnt : {{ totalCnt }}</p>
-    <p>perPage : {{ perPage }}</p>
-    <p>totalPageCnt : {{ totalPageCnt }}</p>
-    <p>firstIndex : {{ firstIndex }}</p>
-    <p>lastIndex : {{ lastIndex }}</p>
-    <p>startPageNo : {{ startPageNo }}</p>
-    <p>endPageNo : {{ endPageNo }}</p>
-    <br>
-    <br>
-    <ul class="flex flex-col gap-y-4">
+    <ul class="flex gap-4">
       <li
         v-for="index in pageIndexes"
         :key="index"
+        class="w-max"
       >
-        <NuxtLinkLocale :to="`/news/${$t(`post.list.${index}.slug`)}`">
-          {{ $t(`post.list.${index}.subject`) }}
-          {{ $t(`post.list.${index}.content`) }}
-          {{ $t(`post.list.${index}.meta_description`) }}
+        <NuxtLinkLocale
+          :to="`/news/${$t(`post.list.${index}.slug`)}`"
+          class="w-[300px] block"
+        >
+          <h2 class="thumbs w-[300px] h-[200px] p-2 surface-color-tertiary text-color-primary grid place-items-center">
+            {{ $t(`post.list.${index}.slug`) }}
+          </h2>
+          <p class="w-full">
+            {{ $t(`post.list.${index}.subject`) }}
+            {{ $t(`post.list.${index}.content`) }}
+            {{ $t(`post.list.${index}.meta_description`) }}
+          </p>
         </NuxtLinkLocale>
       </li>
     </ul>
